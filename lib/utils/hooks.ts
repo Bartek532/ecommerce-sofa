671e53d94b93443445b16af0c569fa48dd9b2728
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-05-20 17:29:13 +0200

    refactorize useWindowSize hook

--- lib/utils/hooks.ts
+++ lib/utils/hooks.ts
@@ -1,26 +1,30 @@
 import { useState, useEffect } from "react";
 
+type Dimensions = { width?: number; height?: number };
+
 export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
+  const [windowSize, setWindowSize] = useState<Dimensions>({
+    width: undefined,
+    height: undefined,
   });
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-
-      window.addEventListener("resize", handleResize);
-
-      handleResize();
-
-      return () => window.removeEventListener("resize", handleResize);
+      setWindowSize({ width: undefined, height: undefined });
     }
+
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   return windowSize;
 };
