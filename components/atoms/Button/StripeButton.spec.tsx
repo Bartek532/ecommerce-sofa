0069341ec57c0fdd9fb936ac38fd237cc85597ef
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-08-01 13:34:10 +0200

    update tests for StripeButton

--- components/atoms/Button/StripeButton.spec.tsx
+++ components/atoms/Button/StripeButton.spec.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { StripeButton } from "./StripeButton";
 
-test("button should be disabled when nothing is in the basket (price = 0)", () => {
+test("button should be disabled when basket is empty (price = 0)", () => {
   render(<StripeButton price={0} />);
   expect(screen.getByRole("button", { name: /pay now/i })).toBeDisabled();
 });
 
-test("button shouldn be enabled when somthing is in the basket (price > 0)", () => {
+test("button should be enabled when basket is not empty (price > 0)", () => {
   render(<StripeButton price={100} />);
   expect(screen.getByRole("button", { name: /pay now/i })).not.toBeDisabled();
 });
