16bc21061405f1e9e6a5168fcd272de167d294b0
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-08-19 14:48:25 +0200

    move sensitive variables to .env

--- lib/datocms/index.ts
+++ lib/datocms/index.ts
@@ -1,3 +1,5 @@
 const SiteClient = require("datocms-client").SiteClient;
 
-export const DatoCMSData = new SiteClient("e22e8b0d12cda7f6f454e1a4fd3f61");
+export const DatoCMSData = new SiteClient(
+  process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN
+);
--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnJUUOefaqT05ccFFNWfBhaeaIkqVE-jM",
-  authDomain: "sofa-app-5c407.firebaseapp.com",
-  projectId: "sofa-app-5c407",
-  storageBucket: "sofa-app-5c407.appspot.com",
-  messagingSenderId: "475598534813",
-  appId: "1:475598534813:web:85511dae761c7d40cc3c74",
-  measurementId: "G-N2TM3QVQCP",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
